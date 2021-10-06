2298217abb3c17557492b6ec6b78fb88617b20a7
cdang5969@gmail.com <cdang5969@gmail.com>
2021-10-06 07:12:55 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NguyenDangCu133.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
